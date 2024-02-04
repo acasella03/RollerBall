4f36aec1a1e3a087ce4aee63daa3c174cfb59b3a
acasella@danielcastelao.org <>
2024-02-04 14:33:48 +0100

    Merge por trabajo en casa

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -22,28 +22,27 @@ public class CameraController : MonoBehaviour
 
     void Update ()
     {
-        //Esto lo comentaría cuando el desplazamiento ya se cumpla con el método LateUpdate,
-        //porque ya no se necesita mover la cámara con el teclado.
-        //if(Input.GetKey(KeyCode.W))
-        //    transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        //Esto lo comentaría cuando el desplazamiento ya se cumpla con el método LateUpdate
+        if(Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
-        //if(Input.GetKey(KeyCode.S))
-        //    transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.S))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
         
-        //Esto es para rotar en el mismo eje del juegador y no alrededor del jugador
-        //if(Input.GetKey(KeyCode.A))
-        //    transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        /* Esto lo comento porque no quiero rotar en un punto sino alrededor
+        if(Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
         
-        //if(Input.GetKey(KeyCode.D))
-        //    transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-       
+        if(Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        */
 
-        //Rotar alredor del jugador
-        //if (Input.GetKey(KeyCode.A))
-        //    transform.RotateAround(player.transform.position, Vector3.up, -turnSpeed * Time.deltaTime);
+        //Rotar alredor
+        if (Input.GetKey(KeyCode.A))
+            transform.RotateAround(player.transform.position, Vector3.up, -turnSpeed * Time.deltaTime);
 
-        //if (Input.GetKey(KeyCode.D))
-        //    transform.RotateAround(player.transform.position, Vector3.up, turnSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.RotateAround(player.transform.position, Vector3.up, turnSpeed * Time.deltaTime);
     }
 
 
@@ -51,7 +50,7 @@ public class CameraController : MonoBehaviour
     void LateUpdate()
     {
         // Mantener el mismo desplazamiento entre la cámara y el jugador a lo largo del juego.
-        transform.position = player.transform.position + offset;
+        //transform.position = player.transform.position + offset;
     }
 }
 
